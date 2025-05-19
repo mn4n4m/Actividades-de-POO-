39add1653c26dc0ae3356a74fa37f5cc1ddb148c
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-19 16:27:28 -0500

    Create PastorAleman.java

---
+++ Actividad # 3/Ejercicio propuesto página 227/PastorAleman.java
@@ -0,0 +1,7 @@
+package TiendaMascotas;
+
+public class PastorAleman extends Perro {
+    public PastorAleman(String nombre, int edad, String color, float peso, boolean muerde) {
+        super(nombre, edad, color, peso, muerde);
+    }
+}
