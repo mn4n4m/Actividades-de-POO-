25aee655bad2d88aa21502adae2c3805d39e696b
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-19 16:33:29 -0500

    Update and rename PastorAleman.java to PerrosRazas.java

--- Actividad # 3/Ejercicio propuesto página 227/PastorAleman.java
+++
@@ -1,7 +0,0 @@
-package TiendaMascotas;
-
-public class PastorAleman extends Perro {
-    public PastorAleman(String nombre, int edad, String color, float peso, boolean muerde) {
-        super(nombre, edad, color, peso, muerde);
-    }
-}
