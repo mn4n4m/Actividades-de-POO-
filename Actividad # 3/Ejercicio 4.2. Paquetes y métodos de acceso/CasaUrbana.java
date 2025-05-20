a2f3e13da67f3a995f227c64d7dba5dcab6bf2fa
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-19 20:16:01 -0500

    Create CasaUrbana.java

---
+++ Actividad # 3/Ejercicio 4.2. Paquetes y métodos de acceso/CasaUrbana.java
@@ -0,0 +1,11 @@
+package Inmuebles;
+
+public class CasaUrbana extends Casa {
+    public CasaUrbana(int id, int area, String direccion, int hab, int banos, int pisos) {
+        super(id, area, direccion, hab, banos, pisos);
+    }
+
+    void imprimir() {
+        super.imprimir();
+    }
+}
