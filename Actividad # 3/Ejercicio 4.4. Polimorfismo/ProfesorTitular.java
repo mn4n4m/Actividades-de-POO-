f91a52484ab3976af2774a7accc36f3479c4a2e4
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-19 19:51:52 -0500

    Create ProfesorTitular.java

---
+++ Actividad # 3/Ejercicio 4.4. Polimorfismo/ProfesorTitular.java
@@ -0,0 +1,8 @@
+package Profesores;
+
+public class ProfesorTitular extends Profesor {
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+}
