b2522e056c6165c5d5d849ac6af5123998635963
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-19 19:52:24 -0500

    Create Prueba.java

---
+++ Actividad # 3/Ejercicio 4.4. Polimorfismo/Prueba.java
@@ -0,0 +1,8 @@
+package Profesores;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Profesor profesor1 = new ProfesorTitular();  // Polimorfismo
+        profesor1.imprimir();  // ¿Qué se imprime?
+    }
+}
